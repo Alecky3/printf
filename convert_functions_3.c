2046989c7d7159d0288df2bc68c02555b402f37f
Jack Mugoya <mugoyajack3@gmail.com>
2022-10-20 21:58:17 +0300

    print_p v0

--- convert_functions_3.c
+++ convert_functions_3.c
@@ -2,3 +2,13 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * print_p - The void * pointer argument is printed in hexadecimal
+ * @args: character argurment to print
+ * Return: number of characters printed
+*/
+
+int print_p(va_list args)
+{
+
+}
