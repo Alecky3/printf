e9a04b40bd6716853c1729ba9911dc1273dfee01
Jack Mugoya <mugoyajack3@gmail.com>
2022-10-20 22:03:19 +0300

    print_l v0

--- convert_functions_3.c
+++ convert_functions_3.c
@@ -12,3 +12,17 @@ int print_p(va_list args)
 {
 
 }
+/**
+ * print_l - (ell)  A  following  integer  conversion corresponds to a long 
+ * or unsigned long argument, or a following n conversion corresponds to a 
+ * pointer to a long argument, or a following c conversion corresponds
+ * to a wint_t argument, or a following s conversion corresponds to a pointer
+ * to wchar_t  argument.
+ * @args: character argurment to print
+ * Return: number of characters printed
+*/
+
+int print_l(va_list args)
+{
+
+}
