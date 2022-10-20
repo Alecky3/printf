51d88bf3ebf5f087937d3e61d2501da100ac5d29
Jack Mugoya <mugoyajack3@gmail.com>
2022-10-20 21:29:01 +0300

    Implemented betty coding and documentation style

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Custom printf function (_printf) which works the same as printf
 main.h conatains function prototypes used in the project
 convert_function.c handles datatypes conversions passed as parameters
-select_function.c  is used to call the appropriate function defined in convert_function.c for datatype conversion 
+select_function.c  is used to call the appropriate function defined in convert_function.c for datatype conversion.
 man3printf is the provided manual page for the standard printf. It was used as a reference to come up with the custom printf.
--- convert_functions_2.c
+++ convert_functions_2.c
@@ -3,27 +3,24 @@
 #include <stdarg.h>
 /**
  * format_b - function to convert to unsigned integer to binary
- * @args: character argurmen to print 
+ * @args: character argurmen to print
  * Return: number of characters printed
  */
 int print_b(va_list args)
 {
-    int count = 0;
-    int c, i, n;
-    char *nums;
+	int count = 0;
+	int c, i, n;
+	char *nums;
 
-    nums = va_arg(args, char*);
+	nums = va_arg(args, char*);
+	int a[10];
 
-    int a[10];
-
-    n = va_arg(args, int);
-    
-    for (i = 0; n > 0; i++)
-    {
-        a[i] = n % 2;
-        n = n / 2;
-    }
-
-    count = (write(STDOUT_FILENO, &c, 1));
-    return (count);
+	n = va_arg(args, int);
+	for (i = 0; n > 0; i++)
+	{
+		a[i] = n % 2;
+		n = n / 2;
+	}
+	count = (write(STDOUT_FILENO, &c, 1));
+	return (count);
 }
